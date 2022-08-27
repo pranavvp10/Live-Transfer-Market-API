f4a00a59e6f22315a0a8f7f899c0ffe13b24a094
Pranav V P <pranavvp07@gmail.com>
2022-08-27 14:21:18 +0530

    added Core, infrastructure

---
+++ LiveTransferMarket.API/LiveTransferMarket.Core/Entities/Club.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiveTransferMarket.Core.Entities
+{
+    public class Club
+    {
+        public int ClubId { get; set; }
+        public string? ClubName { get; set; }
+    }
+}
